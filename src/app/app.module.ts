5a77c220057b3cdf9934696d0af9a5dc30bb0951
Ruben-fg <rubenfernandezgomezbs@gmail.com>
2023-12-04 08:30:39 +0100

    create auth pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { ForgotComponent } from './pages/forgot/forgot.component';
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
+    RegisterComponent,
+    ForgotComponent,
   ],
   imports: [
     BrowserModule,
